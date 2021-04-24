2e846b2c0b2229c24f0fb68f33a6eeac0a2f478f
RebRut03 <78867680+RebRut03@users.noreply.github.com>
2021-04-24 15:13:39 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -5,11 +5,12 @@ SELECT employees.emp_no,
     titles.title,
 	titles.from_date,
 	titles.to_date
-INTO retirement_titles
+--INTO retirement_titles
 FROM employees
-LEFT JOIN titles
+INNER JOIN titles
 ON employees.emp_no = titles.emp_no
-WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no; 
 
 
 -- Use Dictinct with Orderby to remove duplicate rows
@@ -22,10 +23,11 @@ FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 SELECT * FROM unique_titles;
+SELECT * FROM titles;
 
 -- Retrieve the number of employees by their most recent job title who are about to retire.
 SELECT COUNT(title), title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles
 GROUP BY unique_titles.title
 ORDER BY count(title) DESC;
@@ -33,7 +35,7 @@ ORDER BY count(title) DESC;
 SELECT * FROM retiring_titles
 
 -- Create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
-SELECT DISTINCT ON (emp_no) employees.emp_no,
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
     employees.first_name,
 	employees.last_name,
 	employees.birth_date,
@@ -43,14 +45,17 @@ SELECT DISTINCT ON (emp_no) employees.emp_no,
 --INTO mentorship_eligibility
 FROM employees
 INNER JOIN dept_emp
-ON employees.emp_no = dept_emp.emp_no
+ON (employees.emp_no = dept_emp.emp_no)
 INNER JOIN titles
-ON employees.emp_no = titles.emp_no
-WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND (dept_emp.to_date = '9999-01-01')
-ORDER BY emp_no;
+ON (employees.emp_no = titles.emp_no)
+WHERE (dept_emp.to_date = '9999-01-01')
+AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no;
+
 
 SELECT * FROM mentorship_eligibility;
+
+
 
 DROP TABLE broadened_mentorship_eligibility CASCADE;
 SELECT DISTINCT ON (emp_no) employees.emp_no,
@@ -71,3 +76,6 @@ AND (dept_emp.to_date = '9999-01-01')
 ORDER BY emp_no;
 
 SELECT * FROM broadened_mentorship_eligibility;
+
+
+
